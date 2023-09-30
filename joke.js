564a921e736ad15026ff626ad2327b0d76000175
Adedapo <adedapoagbolaha@gmail.com>
2023-09-30 00:15:30 -0700

    first commit

--- joke.js
+++ joke.js
@@ -2,6 +2,7 @@ const jokeContainer = document.getElementById("joke")
 const btn = document.getElementById("btn");
 const wrapper = document.querySelector(".wrapper");
 const sect = document.getElementById("sect");
+const btns = document.getElementById("btns");
 
 const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
 
@@ -12,11 +13,35 @@ let getJoke = () => {
   .then(item => {
     jokeContainer.innerHTML = `${item.joke} &#128514; &#128578; &#128525`
     jokeContainer.classList.add("fade");
-  });
-}
-btn.addEventListener("click", getJoke)
 
-getJoke();
+  let utterance = new SpeechSynthesisUtterance(item.joke);
+  utterance.voice = voices[4];
+  utterance.lang = voices[4].lang;
+  window.speechSynthesis.speak(utterance);
+  
+
+  
+  
+  });
+  
+ 
+}
+
+
+window.speechSynthesis.onvoiceschanged = function() {
+  voices = window.speechSynthesis.getVoices()
+  
+  
+ };
+ function getIt() {
+  window.speechSynthesis.cancel();
+ }
+
+btn.addEventListener("click", getJoke)
+getJoke()
+
+
+
 
 
 function wrap () {
@@ -25,3 +50,23 @@ function wrap () {
   
 
 }
+
+
+
+
+// const text = 'Her name is Simplicity Treasure'
+ 
+
+// function getItt() {
+ 
+  
+//   let utterance = new SpeechSynthesisUtterance(text)
+//  utterance.voice = voices[5];
+//  utterance.lang = voices[4].lang;
+//  window.speechSynthesis.speak(utterance);
+
+// }
+// window.speechSynthesis.onvoiceschanged = function() {
+//   voices = window.speechSynthesis.getVoices()
+  
+//  };
